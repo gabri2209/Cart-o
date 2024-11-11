c6fba420974b9a39c21d2969433826063d46908a
gabri2209 <142520727+gabri2209@users.noreply.github.com>
2024-11-11 09:07:17 -0300

    sasf

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-function criaCartao (categoria, pergunta, resposta) { 
-    let container = document.getElementById('container') 
-    let cartao = document.createElement('article') 
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
     cartao.className = 'cartao'
 
-    cartao.innerHTML =
+    cartao.innerHTML = `
     <div class="cartao__conteudo">
     <h3>${categoria}</h3>
-    <div class="cartao_conteudo_pergunta">
-    <p>${pergunta}</p>
+    <div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
     </div>
-    <div class="cartao_conteudo_resposta">
-    <p>${resposta}</p>
-    </div> 
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
     </div>
+    </div>
+    `
     container.appendChild(cartao)
-    }
+
+}
